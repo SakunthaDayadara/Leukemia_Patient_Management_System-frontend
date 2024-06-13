b4f9dd3db027a461ab891524b24179378a87cd28
Sakuntha <sakunthadayadara2001@gmail.com>
2024-06-13 16:58:29 +0530

    Deployment fix index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import {AuthProvider} from "./Context/AuthProvider";
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-      </LocalizationProvider>
-  </React.StrictMode>
-);
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </LocalizationProvider>
+    </React.StrictMode>
+    , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
